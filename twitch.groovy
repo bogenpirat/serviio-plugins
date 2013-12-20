f5a65edbf4caacc9ef740fd7ec5b889a1f79e2cd
julian <julian8x@gmx.de>
2013-12-20 19:00:47 +0100

    removed RTMP bits

--- twitch.groovy
+++ twitch.groovy
@@ -14,6 +14,10 @@ import groovy.json.*
  *
  *	<h2>VERSION HISTORY</h2>
  *	<p><ul>
+ *		<li>V9 (20.12.2013): removed RTMP streams since they are now disabled
+ *			and will likely be defunct forever.</li>
+ *		<li>V8 (14.12.2013): simplified HLS/mobile grabbing, fixed a bug from
+ *			V5 that broke rtmpurl generation, updated swfUrl</li>
  *		<li>V7 (11.12.2013): changed mobile stream grabber to also get source 
  *			quality</li>
  *		<li>V6 (09.12.2013): added support for displaying mobile streams</li>
@@ -28,17 +32,15 @@ import groovy.json.*
  *		<li>V1 (03.02.2013): initial release</li>
  *	</ul></p>
  *
- *	@version 7
+ *	@version 9
  *	@author <a href="https://twitter.com/bogenpirat">bog</a>
  *
  */
 
 class Twitch extends WebResourceUrlExtractor {
-	final Integer VERSION = 7
-	final String VALID_FEED_URL = /^https?:\/\/(?:[^\.]*.)?(?:twitch|justin)\.tv\/([a-zA-Z0-9_]+).*$/
-	final String TWITCH_API_URL = "http://usher.justin.tv/find/CHANNELNAME.json?type=any&group=&channel_subscription="
+	final Integer VERSION = 9
+	final String VALID_FEED_URL = /^https?:\/\/(?:[^\.]*.)?(?:twitch|justin)\.tv\/([a-zA-Z0-9_]+).*$/ // TODO
 	final String TWITCH_HLS_API_PLAYLIST_URL = "http://usher.twitch.tv/select/CHANNELNAME.json?allow_source=true&nauthsig=&nauth=&type=any"
-	final String TWITCH_SWF_URL = "http://www.justin.tv/swflibs/JustinPlayer.swf?channel="
 	final static Boolean isWindows = System.getProperty("os.name").startsWith("Windows");
 	
 	int getVersion() {
@@ -48,79 +50,19 @@ class Twitch extends WebResourceUrlExtractor {
 	String getExtractorName() {
 		return 'twitch.tv'
 	}
-	
-	/**
-	 * resolves the URL of the SWF file that would normally play the video.
-	 * this is taken from the Location:-header that the server supplies with its
-	 * 302 response upon requesting TWITCH_SWF_URL
-	 * @param channelName name of the justin.tv/twitch.tv channel
-	 */
-	String getSwfUrl(String channelName) {
-		def url = new URL(TWITCH_SWF_URL + channelName)
-		HttpURLConnection con = (HttpURLConnection) url.openConnection()
-		con.setInstanceFollowRedirects(false)
-		if(con.getResponseCode() % 300 < 100)
-			return con.getHeaderField("Location").replaceAll("\\?.*", "")
-		else
-			return TWITCH_SWF_URL
-	}
 
 	boolean extractorMatches(URL feedUrl) {
 		return feedUrl ==~ VALID_FEED_URL
 	}
 	
 	WebResourceContainer extractItems(URL resourceUrl, int maxItemsToRetrieve) {
-		////////////////////////////
-		// REGULAR RTMP INTERFACE //
-		////////////////////////////
-		// let's set some required variables
 		def channelName = (String) (resourceUrl =~ VALID_FEED_URL)[0][1] // extract channel name from url
-		def live = 1 // for ffmpeg rtmp parameters
-		def swfUrl = getSwfUrl(channelName)
-		
-		// grab and parse the api output and isolate the items
-		def jsonText = new URL(TWITCH_API_URL.replaceAll("CHANNELNAME", channelName.toLowerCase())).text
-		def json = new JsonSlurper().parseText(jsonText)
 		
 		def items = [] // prepare list
 		
-		if(json.size() > 0) // check that api response isn't empty (i.e. stream is offline)
-			json.each {
-				// it.connect is "rtmp://someip/app", so it already includes the "app" parameter
-				def rtmp = it.connect
-				if(rtmp == null || !rtmp.startsWith("rtmp"))
-					return // likely not what we want
-				def playpath = it.play
-				def jtv
-				def expiration
-				if((it.token == "" || it.token == null) && it.connect ==~ /.*\d+\.\d+\.\d+\.\d+.*/) {
-					log "skipping quality ${it.type} because it has no token and requires one" 
-					return // skip qualities where we get no token (subscription) and it's a non-cdn server
-				} 
-				if(it.token != null) {
-					// exchange the jtv token's spaces (depending on OS), escape backslashes
-					if(isWindows)
-						jtv = it.token.replaceAll("\"", "\\\\\"")
-					else
-						jtv = it.token.replaceAll("\"", "\\\"")
-					jtv = jtv.replaceAll(" ", "\\\\20")
-					
-					expiration = Integer.parseInt((it.token =~ /.*"expiration": (\d+)[^\d].*/)[0][1])
-				}
-				// a generic string should be enough for identifying purposes
-				def title = channelName + "-" + it.type + " [${it.video_height}p]"
-				
-				items += new WebResourceItem(title: title, additionalInfo: [ 
-					expiresImmediately: true,
-					cacheKey: title,
-					// required parameters: rtmp-url, playpath, swfUrl/Vfy, live, jtv (CDN servers don't need this)
-					rtmpUrl: rtmp + " playpath=" + playpath + " swfUrl=" + swfUrl + " swfVfy=1" + ((rtmp ==~ /.*\d+\.\d+\.\d+\.\d+.*/)? " jtv=" + jtv : "") + " live=" + live ])
-			}
-		
-			
-		//////////////////////////
-		// MOBILE HLS INTERFACE //
-		//////////////////////////
+		/////////////////////////
+		// HLS VIDEO INTERFACE //
+		/////////////////////////
 		def playlist = new URL(TWITCH_HLS_API_PLAYLIST_URL.
 			replaceAll("CHANNELNAME", channelName.toLowerCase())).text
 		
@@ -132,7 +74,7 @@ class Twitch extends WebResourceUrlExtractor {
 			items += new WebResourceItem(title: title, additionalInfo: [ 
 				expiresImmediately: true,
 				cacheKey: title,
-				rtmpUrl: m.group(3) ])
+				url: m.group(3) ])
 		}
 		
 		
@@ -149,7 +91,7 @@ class Twitch extends WebResourceUrlExtractor {
 		if(arg0 != null) {
 			c.setExpiresImmediately(true)
 			c.setCacheKey(arg0.additionalInfo.cacheKey)
-			c.setContentUrl(arg0.additionalInfo.rtmpUrl)
+			c.setContentUrl(arg0.additionalInfo.url)
 			c.setLive(true)
 		}
 		return c
